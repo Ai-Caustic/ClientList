9fe54745690de44194161a4ec9ddeef8693289a4
Victor Micheni <victormichenim@gmail.com>
2023-05-17 16:26:55 +0300

    Added create new client page and code

--- Pages/Clients/Index.cshtml.cs
+++ Pages/Clients/Index.cshtml.cs
@@ -7,39 +7,32 @@ namespace ClientList.Pages.Clients
 {
     public class IndexModel : PageModel
     {
-        public List<ClientInfo> listClients = new List<ClientInfo>();
+        public List<ClientInfo> listClients = new();
         public void OnGet()
         {
             try
             {
-                String connectionString = "Data Source=CAUSTIC\\CAUSTIC;Initial Catalog=mystore;Integrated Security=True";
+                string connectionString = "Data Source=CAUSTIC\\CAUSTIC;Initial Catalog=mystore;Integrated Security=True";
 
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using SqlConnection connection = new(connectionString);
+                connection.Open();
+                string sql = "SELECT * FROM clients";
+                using SqlCommand command = new(sql, connection);
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
                 {
-                    connection.Open();
-                    String sql = "SELECT * FROM clients";
-                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    ClientInfo clientinfo = new()
                     {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read()) {
-                                ClientInfo clientinfo = new ClientInfo
-                                {
-                                    id = "" + reader.GetInt32(0),
-                                    name = reader.GetString(1),
-                                    email = reader.GetString(2),
-                                    phone = reader.GetString(3),
-                                    address = reader.GetString(4),
-                                    created_at = reader.GetDateTime(5).ToString()
-                                    
-                                };
-                                listClients.Add(clientinfo);
-                                Console.WriteLine(clientinfo);
-                            }
-                         
-                            
-                        }
-                    }
+                        id = "" + reader.GetInt32(0),
+                        name = reader.GetString(1),
+                        email = reader.GetString(2),
+                        phone = reader.GetString(3),
+                        address = reader.GetString(4),
+                        created_at = reader.GetDateTime(5).ToString()
+
+                    };
+                    listClients.Add(clientinfo);
+                    Console.WriteLine(clientinfo);
                 }
             }
             catch (Exception ex) {
@@ -50,11 +43,11 @@ namespace ClientList.Pages.Clients
 
     public class ClientInfo
     {
-        public String id;
-        public String name;
-        public String email;
-        public String phone;
-        public String address;
-        public String created_at;
+        public string id;
+        public string name;
+        public string email;
+        public string phone;
+        public string address;
+        public string created_at;
     }
 }
