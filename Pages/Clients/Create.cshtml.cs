ff8f2598cdc5178213d9ae930210ae598a1b9f4b
Victor Micheni <victormichenim@gmail.com>
2023-05-18 17:15:15 +0300

    Added Edit and delete pages

--- Pages/Clients/Create.cshtml.cs
+++ Pages/Clients/Create.cshtml.cs
@@ -31,21 +31,31 @@ namespace ClientList.Pages.Clients
                 string connectionString = "Data Source=CAUSTIC\\CAUSTIC;Initial Catalog=mystore;Integrated Security=True";
                 using SqlConnection connection = new(connectionString);
                 connection.Open();
+                string sqlCmd = "INSERT INTO clients" + "(name, email, phone, address) VALUES" + "(@name, @email, @phone, @address)";
+                using SqlCommand cmd = new SqlCommand(sqlCmd, connection);
+                    cmd.Parameters.AddWithValue("@name", clientinfo.name);
+                    cmd.Parameters.AddWithValue("@email", clientinfo.email);
+                    cmd.Parameters.AddWithValue("@phone", clientinfo.phone);
+                    cmd.Parameters.AddWithValue("@address", clientinfo.address);
 
-                
+                    cmd.ExecuteNonQuery();
+
             }
             catch (Exception ex)
             {
 
                 errorMessage = ex.Message;
+                return;
             }
 
-            //Save the Client to the database
+            //Save the new Client to the database
             clientinfo.name = "";
             clientinfo.email = "";
             clientinfo.phone = "";
             clientinfo.address = "";
             successMessage = "New client added succesfully";
+
+            Response.Redirect("/Clients/index");
 
 
         }    
